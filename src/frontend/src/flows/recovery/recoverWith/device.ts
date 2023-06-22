f27615861edbdf6a92e6115bbb2d45182555d672
Nicolas Mattia <nicolas.mattia@dfinity.org>
2023-06-22 15:48:11 +0200

    wip

--- src/frontend/src/flows/recovery/recoverWith/device.ts
+++ src/frontend/src/flows/recovery/recoverWith/device.ts
@@ -2,9 +2,7 @@ import { promptUserNumberTemplate } from "$src/components/promptUserNumber";
 import { toast } from "$src/components/toast";
 import {
   apiResultToLoginFlowResult,
-  LoginFlowCanceled,
-  LoginFlowError,
-  LoginFlowSuccess,
+  LoginFlowResult,
 } from "$src/utils/flowResult";
 import { Connection } from "$src/utils/iiConnection";
 import { renderPage } from "$src/utils/lit-html";
@@ -31,7 +29,7 @@ export const recoverWithDevice = ({
 }: {
   connection: Connection;
 }): Promise<
-  LoginFlowSuccess | LoginFlowError | LoginFlowCanceled /* TODO: squash */
+  LoginFlowResult
 > => {
   return new Promise((resolve) => {
     return recoverWithDevicePage({
@@ -62,7 +60,7 @@ const attemptRecovery = async ({
 }: {
   userNumber: bigint;
   connection: Connection;
-}): Promise<LoginFlowSuccess | LoginFlowError | LoginFlowCanceled> => {
+}): Promise<LoginFlowResult> => {
   const { recovery_credentials: recoveryCredentials } =
     await connection.lookupCredentials(userNumber);
 
@@ -73,7 +71,9 @@ const attemptRecovery = async ({
   }
 
   if (recoveryCredentials.length > 1) {
-    toast.error("More than one recovery device found, this is unexpected");
+    const title = "Too many recovery devices";
+    const message = "This identity has more than one recovery devices, which is not expected";
+    return { tag: "err", title, message };
   }
 
   const result = apiResultToLoginFlowResult(
