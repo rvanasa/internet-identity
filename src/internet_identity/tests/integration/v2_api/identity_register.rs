357bd9a90ef9fa661656cae45ede00f55d9a4612
Frederik Rothenberger <frederik.rothenberger@dfinity.org>
2023-08-29 16:09:41 +0200

    wip

--- src/internet_identity/tests/integration/v2_api/identity_register.rs
+++ src/internet_identity/tests/integration/v2_api/identity_register.rs
@@ -2,18 +2,28 @@ use crate::v2_api::authn_method_test_helpers::{
     create_identity_with_authn_method, test_authn_method,
 };
 use canister_tests::framework::{
-    arg_with_anchor_range, env, install_ii_canister_with_arg, II_WASM,
+    arg_with_anchor_range, env, install_ii_canister, install_ii_canister_with_arg, II_WASM,
 };
 use ic_test_state_machine_client::CallError;
 
-/// Tests user registration with cross checks for get_anchor_credentials, get_anchor_info and get_principal.
 #[test]
-fn should_register_new_anchor() -> Result<(), CallError> {
+fn should_register_new_identity() -> Result<(), CallError> {
     let env = env();
     let canister_id =
         install_ii_canister_with_arg(&env, II_WASM.clone(), arg_with_anchor_range((1, 2)));
     let authn_method = test_authn_method();
-    let user_number = create_identity_with_authn_method(&env, canister_id, &authn_method);
-    assert_eq!(user_number, 1);
+    let identity_number = create_identity_with_authn_method(&env, canister_id, &authn_method);
+    assert_eq!(identity_number, 1);
     Ok(())
+}
+
+#[test]
+fn should_register_multiple_identities() {
+    let env = env();
+    let canister_id = install_ii_canister(&env, II_WASM.clone());
+    let authn_method = test_authn_method();
+    let identity_number_1 = create_identity_with_authn_method(&env, canister_id, &authn_method);
+    let identity_number_2 = create_identity_with_authn_method(&env, canister_id, &authn_method);
+
+    assert_ne!(identity_number_1, identity_number_2);
 }
